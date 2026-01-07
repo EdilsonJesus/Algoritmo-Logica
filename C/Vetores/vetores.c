8228f9b96b04bc7ba8fe105349bb131dde24c020
Edilson Jesus <edilsondevelop@gmail.com>
2026-01-07 20:01:29 -0300

    Produção initicial

---
+++ C/Vetores/vetores.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main () {
+    
+    int N, i; 
+
+    printf("Quantos números você vai digitar? ");
+    scanf("%d", &N);
+    
+    double vet[N];
+
+    for (i = 0; i < N; i++ ) {
+        printf("Digite um número: ");
+        scanf("%lf", &vet[i]);
+    }
+
+    printf("Números digitados: \n");
+
+    for (i = 0; i < N; i++ ) {
+        printf("%.1lf\n", vet[i]); 
+    }
+
+    return 0;
+}
