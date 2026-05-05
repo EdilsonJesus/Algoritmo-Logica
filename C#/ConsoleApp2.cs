3d62ad3bf1b7fd197813e0e3f408b789d4463272
Edilson Jesus <edilsondevelop@gmail.com>
2026-05-05 15:44:49 -0300

    Realizando correções de arquivos

---
+++ C#/ConsoleApp2.cs
@@ -0,0 +1,11 @@
+using System; 
+
+namespace Primeiro{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            Console.WriteLine("Hello World !!");
+        }
+    }
+}
---
+++ C++/Estrura_Inicial/Entrada_de_Dados.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h> // essa biblioteca agrega varias outras 
+
+using namespace std;
+int main (){
+    double salario1, salario2; 
+    string nome1, nome2; 
+    int idade;
+    char sexo;
+
+    cout << "Nome da primeira pessoa: ";
+    getline(cin, nome1);
+    cout << "Salario da primeira pessoa: ";
+    cin >> salario1;
+
+    
+    cout << "Nome da segunda pessoa: ";
+    
+
+    return 0;
+} // namespace std;
